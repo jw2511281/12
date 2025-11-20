af7f21e7f595c04561fca16f743998799ed30e3a
jw2511281 <jw2511281@sookmyung.ac.kr>
2025-11-20 19:16:47 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <board.h>
+
+int main(int argc, char *argv[])
+{
+  srand( (unsigned) (time(NULL)) );
+  //opening
+  printf("____________________________________________\n");
+  printf("быбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбы\n");
+  printf("в╞               GAME STRAT                в╞\n");
+  printf("быбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбы\n");
+  printf("============================================\n\n\n");
+  
+  //step 1. initialization (player name setting, variables)
+  
+  //step 2. tunr play (do-while)
+  // 2-1. status printing
+  // 2-2. roll dice
+  // 2-3. move (result)
+  // 2-4. change turn, shark move
+  
+  //step 3. game end (winner printing)
+  
+  
+  //ending
+  printf("\n\n\n\n\n\n\n\n");
+  printf("____________________________________________\n");
+  printf("быбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбы\n");
+  printf("в╞               GAME END                  в╞\n");
+  printf("быбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбы\n");
+  printf("============================================\n");
+  
+  system("PAUSE");	
+  return 0;
+}
+ 
