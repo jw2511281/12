1f62dc3297f7d98b7240ccdb5407169b782190c0
jw2511281 <jw2511281@sookmyung.ac.kr>
2025-11-20 21:36:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include <board.h>
+#include "board.h"
+
+#define MAX_DICE           6
+
+int rolldice(void)
+{
+    int dice= rand() % MAX_DICE+1;
+    return dice;
+}
 
 int main(int argc, char *argv[])
 {
+  int cnt;
+  int pos;
+  
   srand( (unsigned) (time(NULL)) );
   //opening
   printf("____________________________________________\n");
   printf("быбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбы\n");
-  printf("в╞               GAME STRAT                в╞\n");
+  printf("в╞               GAME START                в╞\n");
   printf("быбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбыбы\n");
   printf("============================================\n\n\n");
   
   //step 1. initialization (player name setting, variables)
+  board_initBoard();
   
   //step 2. tunr play (do-while)
-  // 2-1. status printing
-  // 2-2. roll dice
-  // 2-3. move (result)
-  // 2-4. change turn, shark move
+  cnt = 0;
+  pos = 0;
   
+  do {
+     int die_result;
+     int coin_result;
+      
+     // 2-1. status printing
+     board_printBoardStatus();
+     
+     // 2-2. roll dice
+     die_result = rolldice();
+     
+     // 2-3. move (result)
+     pos+= die_result;
+     
+     printf("pos : %i (dice: %i )\n", pos, die_result);
+     
+     coin_result = board_getBoardCoin(pos);
+     
+     printf("coin: %i\n", coin_result);
+     
+     // 2-4. change turn, shark move
+     cnt++;
+  }
+  while(cnt<5);
   //step 3. game end (winner printing)
   
   
